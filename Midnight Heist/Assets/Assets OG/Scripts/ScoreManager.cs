39114511bfe56d6f9ff7871a44398e902a228232
Treasurek04 <treasurekeys2004@gmail.com>
2024-10-07 14:06:44 -0500

    Room 2 and 3 Done
    Just need UI text for the score

---
+++ Midnight Heist/Assets/Assets OG/Scripts/ScoreManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text scoreText;
+    private int currentScore = 0;
+
+    public void AddScore(int scoreToAdd)
+    {
+        currentScore += scoreToAdd;
+        UpdateScoreUI();
+    }
+
+    private void UpdateScoreUI()
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = "Score: " + currentScore.ToString();
+        }
+    }
+
+    public void ResetScore()
+    {
+        currentScore = 0;
+        UpdateScoreUI();
+    }
+
+    public int GetScore()
+    {
+        return currentScore;
+    }
+}

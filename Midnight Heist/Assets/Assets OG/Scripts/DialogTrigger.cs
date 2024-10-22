4a94da4f392606202cddfd9a78d6cde17796abed
TheGreenStarCollector <tptas5337@365.elmhurst.edu>
2024-10-22 13:54:43 -0500

    Working on dialog.

--- Midnight Heist/Assets/Assets OG/Scripts/DialogTrigger.cs
+++ Midnight Heist/Assets/Assets OG/Scripts/DialogTrigger.cs
@@ -1,43 +1,63 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DialogTrigger : MonoBehaviour
 {
-    public DialogManager dialogManager;
-    public Canvas dialogCanvas;
-    private bool inZone = false;
-    
+    public Canvas dialogCanvas; // Assign the Canvas in the Inspector
+    private PlayerController playerController;
+    private bool isInDialogZone = false;
+
     void Start()
     {
-        dialogCanvas.gameObject.SetActive(false);
+        dialogCanvas.gameObject.SetActive(false); // Make sure the Canvas is initially hidden
     }
-    
+
     void Update()
     {
-        if (inZone && Input.GetKeyDown(KeyCode.E))
+        if (isInDialogZone && Input.GetKeyDown(KeyCode.T))
         {
-            dialogCanvas.gameObject.SetActive(true);
-            dialogManager.gameObject.SetActive(true);
-            dialogManager.StartCoroutine("Type");
+            ShowDialog();
         }
     }
-    
-    private void OnTriggerEnter(Collider other)
+
+    void ShowDialog()
+    {
+        // Set player movement to false
+        if (playerController != null)
+        {
+            playerController.canMove = false;
+        }
+
+        // Show the dialog Canvas
+        dialogCanvas.gameObject.SetActive(true);
+    }
+
+    public void HideDialog()
+    {
+        // Hide the dialog Canvas
+        dialogCanvas.gameObject.SetActive(false);
+
+        // Set player movement back to true
+        if (playerController != null)
+        {
+            playerController.canMove = true;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            inZone = true;
+            playerController = other.GetComponent<PlayerController>();
+            isInDialogZone = true;
         }
     }
-    
-    private void OnTriggerExit(Collider other)
+
+    void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            inZone = false;
-            dialogCanvas.gameObject.SetActive(false);
-            dialogManager.SkipSentences();
+            isInDialogZone = false;
+            HideDialog(); // Hide dialog if the player leaves the zone
         }
     }
 }

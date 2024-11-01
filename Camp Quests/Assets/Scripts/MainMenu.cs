a92c39e00d78184df2256a0c166e7bcb1b8b6c1c
stefch07 <schar9360@365.elmhurst.edu>
2024-11-01 10:53:49 -0500

    started working on the main menu

---
+++ Camp Quests/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("TutorialScene");
+    }
+    
+    public void PlayRealGame()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+
+    public void GoToOptionsMenu()
+    {
+        SceneManager.LoadScene("OptionsMenu");
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void CreditsButton()
+    {
+        SceneManager.LoadScene("Credits"); // scene 4 in case it doesn't work
+    }
+
+    public void GoToLevelSelect()
+    {
+        SceneManager.LoadScene("LevelSelector");
+    }
+}

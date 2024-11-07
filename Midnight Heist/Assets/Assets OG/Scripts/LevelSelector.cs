2182e17b60c471a0fdb1e7811bbe147b32ff9437
TheGreenStarCollector <tptas5337@365.elmhurst.edu>
2024-11-07 15:55:44 -0600

    Main menu work.

--- Midnight Heist/Assets/Assets OG/Scripts/LevelSelector.cs
+++ Midnight Heist/Assets/Assets OG/Scripts/LevelSelector.cs
@@ -5,12 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class LevelSelector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     public void OpenScene()
     {
         SceneManager.LoadScene("TutorialScene");

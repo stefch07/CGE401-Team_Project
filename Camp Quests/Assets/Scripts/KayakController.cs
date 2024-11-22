b1ae76713f38292e31d76743e055508e34efc950
TheGreenStarCollector <tptas5337@365.elmhurst.edu>
2024-11-22 17:21:46 -0600

    Kayak minigame.

--- Camp Quests/Assets/Scripts/KayakController.cs
+++ Camp Quests/Assets/Scripts/KayakController.cs
@@ -7,6 +7,8 @@ public class KayakController : MonoBehaviour
     public float verticalInput;
     public float speed = 10.0f;
     public float yRange = 2.5f;
+    public float val1 = 1.0f;
+    public float val2 = 1.0f;
     
     public AudioSource backgroundMusic;
     
@@ -20,12 +22,12 @@ public class KayakController : MonoBehaviour
         transform.Translate(Vector3.up * verticalInput * Time.deltaTime * speed);
         
         // keep player in bounds
-        if (transform.position.y < (-yRange - 1.0f)) {
-            transform.position = new Vector3(transform.position.x, (-yRange - 1.0f), transform.position.z);
+        if (transform.position.y < (-yRange - val1)) {
+            transform.position = new Vector3(transform.position.x, (-yRange - val1), transform.position.z);
         }
         
-        if (transform.position.y > (yRange - 1.0f)) {
-            transform.position = new Vector3(transform.position.x, (yRange - 0.5f), transform.position.z);
+        if (transform.position.y > (yRange - val2)) {
+            transform.position = new Vector3(transform.position.x, (yRange - val2), transform.position.z);
         }
     }
 }

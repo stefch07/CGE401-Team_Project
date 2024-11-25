7bfcd99ada78b658bae21e4d45146debaa3a8c41
Treasurek04 <treasurekeys2004@gmail.com>
2024-11-25 16:03:57 -0600

    Working on background for the hiking scene

---
+++ Camp Quests/Assets/Scripts/HikingScripts/RepeatingBackground.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBackground : MonoBehaviour
+{
+    private Vector3 startPos;
+    private float repeatWidth;
+
+    private void Start()
+    {
+        startPos = transform.position;
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
+    }
+
+    private void Update()
+    {
+        if (transform.position.x < startPos.x - repeatWidth)
+        {
+            transform.position = startPos;
+        }
+    }
+
+}

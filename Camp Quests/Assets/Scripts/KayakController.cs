71eddebeaff2ecf6115e602be9f79af88c0149cf
TheGreenStarCollector <tptas5337@365.elmhurst.edu>
2024-11-29 15:20:27 -0600

    Horizontal input.

--- Camp Quests/Assets/Scripts/KayakController.cs
+++ Camp Quests/Assets/Scripts/KayakController.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class KayakController : MonoBehaviour
 {
     public float verticalInput;
+    public float horizontalInput;
     public float speed = 10.0f;
+    public float xRange = 5.0f;
     public float yRange = 2.5f;
     public float val1 = 1.0f;
     public float val2 = 1.0f;
@@ -15,13 +17,23 @@ public class KayakController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Get vertical input
+        horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
         
-        // transform vertically with input
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
         transform.Translate(Vector3.up * verticalInput * Time.deltaTime * speed);
         
         // keep player in bounds
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        
         if (transform.position.y < (-yRange - val1)) {
             transform.position = new Vector3(transform.position.x, (-yRange - val1), transform.position.z);
         }
